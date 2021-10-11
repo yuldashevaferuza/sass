6346ce5071b850a4d4b1b2fbe4f54e5e3e66fb24
yuldashevaferuza <feruzauldase@gmail.com>
2021-10-11 16:12:56 +0300

    initial

---
+++ js/jquery.js
@@ -0,0 +1,1 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+$ (".slider") .slick({
+    arrows:true,
+    slidesToShow:1,
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true
+})
